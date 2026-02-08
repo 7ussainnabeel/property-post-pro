4b6cea133e5e22137e4efb95dc94c440191341ed
7ussain_nabeel <hussain.dahi@outlook.com>
2026-02-08 10:13:33 +0300

    fix: Remove extraneous character in useEffect dependency array

--- src/pages/DeletedDescriptions.tsx
+++ src/pages/DeletedDescriptions.tsx
@@ -75,7 +75,7 @@ export default function DeletedDescriptions() {
 
   useEffect(() => {
     fetchDeletedItems();
-  }, [showAllBranches]););
+  }, [showAllBranches]);
 
   useEffect(() => {
     filterItems();
